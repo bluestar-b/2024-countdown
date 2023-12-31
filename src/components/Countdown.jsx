b9df9ad6e34dc01f2e9282e7a169bb15628ee7dc
bluestar_ <canikissfemboy@gmail.com>
2023-12-31 13:37:03 +0700

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,18 +1,31 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Snowfall from "react-snowfall";
 import { TimezoneDropdown } from "./TimezoneDropdown";
 import { calculateTimeLeft } from "../lib/calculateTimeLeft";
 import moment from "moment-timezone";
 
+
+
+
+
+
+
+
 export const Countdown = () => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft("UTC"));
   const [isNewYear, setIsNewYear] = useState(false);
   const [timezone, setTimezone] = useState(moment.tz.guess());
+  const [currentTime, setCurrentTime] = useState(moment().tz(timezone).format("HH:mm:ss"));
+  const [currentDate, setCurrentDate] = useState(moment().tz(timezone).format("YYYY-MM-DD"));
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer = setInterval(() => {
       const newTimeLeft = calculateTimeLeft(timezone);
       setTimeLeft(newTimeLeft);
+
+      const now = moment().tz(timezone);
+      setCurrentTime(now.format("HH:mm:ss"));
+      setCurrentDate(now.format("YYYY-MM-DD"));
 
       if (
         newTimeLeft.days === 0 &&
@@ -24,7 +37,7 @@ export const Countdown = () => {
       }
     }, 1000);
 
-    return () => clearTimeout(timer);
+    return () => clearInterval(timer);
   }, [timezone, timeLeft]);
 
   const handleTimezoneChange = (selectedTimezone) => {
@@ -34,55 +47,50 @@ export const Countdown = () => {
   const addLeadingZero = (value) => (value < 10 ? `0${value}` : value);
 
   const timezoneOptions = moment.tz.names();
-  const numberClass = "text-3xl font-bold"
+  const numberClass = "text-3xl font-bold";
+
   return (
     <div className="flex items-center justify-center h-screen relative">
-      <div className="absolute top-4 left-4">
+      <div className="absolute top-4 left-4 flex flex-col items-center">
+        <div className="fixed right-2">
+          <p className="text-lgxl font-semibold">{currentDate} {currentTime}</p>
+        </div>
+        
         <TimezoneDropdown
           value={timezone}
           onChange={handleTimezoneChange}
           options={timezoneOptions}
         />
+  
       </div>
 
-      <Snowfall
-        color="#FFF"
-        snowflakeCount={10}
-        className="absolute"
-        radius={[1, 5]}
-      />
-      <div className="text-center relative z-20">
-        <h1 className="text-4xl font-bold mb-4">COUNTDOWN TO 2024</h1>
-        {isNewYear ? (
+      <div className="flex flex-col items-center">
+        <Snowfall
+          color="#FFF"
+          snowflakeCount={10}
+          className="absolute"
+          radius={[1, 5]}
+        />
+        <div className="text-center relative z-20">
+          <h1 className="text-4xl font-bold mb-4">COUNTDOWN TO 2024</h1>
           <div>
-            <p className="text-2xl font-semibold">Happy New Year!</p>
+            <p className="text-2xl font-semibold">{isNewYear ? "Happy New Year!" : ""}</p>
           </div>
-        ) : (
-          <div>
-            {timeLeft.days > 0 && (
-              <div>
-                <span className={numberClass}>
-                  {addLeadingZero(timeLeft.days)}
-                </span>{" "}
-                days{" "}
-              </div>
-            )}
+          {!isNewYear && (
             <div>
-              <span className={numberClass}>
-                {addLeadingZero(timeLeft.hours)}
-              </span>{" "}
-              hours{" "}
-              <span className={numberClass}>
-                {addLeadingZero(timeLeft.minutes)}
-              </span>{" "}
-              minutes{" "}
-              <span className={numberClass}>
-                {addLeadingZero(timeLeft.seconds)}
-              </span>{" "}
-              seconds
+              {timeLeft.days > 0 && (
+                <div>
+                  <span className={numberClass}>{addLeadingZero(timeLeft.days)}</span> days{" "}
+                </div>
+              )}
+              <div>
+                <span className={numberClass}>{addLeadingZero(timeLeft.hours)}</span> hours{" "}
+                <span className={numberClass}>{addLeadingZero(timeLeft.minutes)}</span> minutes{" "}
+                <span className={numberClass}>{addLeadingZero(timeLeft.seconds)}</span> seconds
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
